9494b42cf942ee2b5b94130804653b49beefb558
Kmn04 <21zaarin01@gmail.com>
2023-04-30 14:23:01 +0300

    немного красоты

--- routes/projects.mjs
+++ routes/projects.mjs
@@ -1,17 +1,14 @@
 import express from "express";
 import { Projects } from "../models/Projects.mjs";
 import { Project_relations } from "../models/Project_relations.mjs";
-import Sequelize from "sequelize";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
     const allUserProjects = await Project_relations.findAll({
         where: {
-            [Sequelize.Op.and]: [
-                {user: req.body.user.user_id },
-                {deleted_on: null}
-            ]
+            user: req.body.user.user_id,
+            deleted_on: null,
         },
         include: {
             model: Projects,
@@ -21,9 +18,9 @@ router.get("/", async (req, res) => {
     res.send(allUserProjects).status(200);
 });
 
-router.get("/project", async (req, res) => {
-    const project = await Projects.findByPk(req.body.project_id);
-    if ((await Project_relations.findByPk(project.project_id)).user === req.body.user.user_id){
+router.get("/:project_id", async (req, res) => {
+    if ((await Project_relations.findByPk(req.params.project_id)).user === req.body.user.user_id){    
+        const project = await Projects.findByPk(req.params.project_id);
         res.send(project).status(200);
     } else{
         alert ('Не хватает прав');
@@ -47,17 +44,9 @@ router.post("/create", async (req, res) => {
 });
 
 router.put("/edit", async(req, res) => {
-    await (await Projects.findByPk(req.body.project_id)).update({
-        name: req.body.name,
-        date_start: req.body.date_start,
-        date_finish: req.body.date_finish,
-        description: req.body.description,
-        status: req.body.status,
-        budget: req.body.budget,
-        currency: req.body.currency,
-        sum_hours_plan: req.body.sum_hours_plan,
-        sum_hours_fact: req.body.sum_hours_fact
-    });
+    const tempProject = await Projects.findByPk(req.body.project_id); 
+    const {owner, user, ...newBody} = req.body;
+    await tempProject.update(newBody);
     res.send().status(200);
 });
 
