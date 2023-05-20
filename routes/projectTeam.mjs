64a1507ad9e5f995012c68e77ff61b5916eb429e
kmn04 <21zaarin01@gmail.com>
2023-05-20 22:26:01 +0300

    админка участников/ролей на проекте

--- routes/projectTeam.mjs
+++ routes/projectTeam.mjs
@@ -1,35 +1,170 @@
-// на проекте только 1 роль
-// при смене роли либо менять роль либо удалять и делать новую, но наверно нахрен не нужно плодить столько
-
-// Получение команды проекта
-
 import express from "express";
 import { ProjectTeamMember } from "../models/ProjectTeamMember.mjs";
-import { User } from "../models/User.mjs";
-import { Role } from "../models/Role.mjs";
-
+import {Role} from "../models/Role.mjs";
+import {Project} from "../models/Project.mjs";
+import {User} from "../models/User.mjs";
+import {Op} from 'sequelize'
 
 const router = express.Router();
-router.get("/:projectId/members", async (req, res) => {
-    const tempMembers = await ProjectTeamMember.findAll({
+
+
+// получить список участников команды на проекте
+router.get("/:projectId/team", async (req, res) => {
+    const actualRole = await ProjectTeamMember.findOne({
         where: {
             projectId: req.params.projectId,
-            finishedAt: null,
-        },
-        include: [{
-            model: User,
-            attributes: ["firstName", "secondName", "login", "userId", "email"]
-        }, Role],
-    })
-    const result = tempMembers.map((member) => {
-        return {
-            userId: member.dataValues.userId,
-            user: member.dataValues.user.dataValues,
-            roleCode: member.dataValues.roleCode,
-            roleName: member.dataValues.role.dataValues.name
+            userId: req.body.user.userId,
+            finishedAt: null
         }
-    })
-    res.send(result)
-})
+    });
+    if (actualRole) {
+        const allProjectTeamMembers = await ProjectTeamMember.findAll({
+            where: {
+                projectId: req.params.projectId,
+                finishedAt: null,
+            },
+            include: [
+                {
+                    model: Project,
+                    attributes: ['name']
+                },
+                {
+                    model: User,
+                    attributes: ['secondName', 'firstName', 'thirdName']
+                },
+                {
+                    model: Role,
+                    attributes: ['name', 'description']
+                }
+            ]
+        });
+        const preparedResult = allProjectTeamMembers.map((teamModel) => {
+            return {
+                projectName: teamModel.project.name,
+                projectId: teamModel.dataValues.projectId,
+                projectTeamMemberId: teamModel.dataValues.projectTeamMemberId,
+                userId: teamModel.dataValues.userId,
+                // сделать join массива
+                projectMember: teamModel.user.secondName + " " + teamModel.user.firstName + " " + (teamModel.user.thirdName ?? ''),
+                roleCode: teamModel.dataValues.roleCode,
+                roleName: teamModel.role.name,
+                roleDescription: teamModel.role.description,
+            }
+        })
+        res.send(preparedResult).status(200);
+    }
+    else {
+        const err = new Error("У вас нет доступа для просмотра команды этого проекта");
+        res.status(400);
+        res.send(err).status(400);
+    }
+});
+
+
+// добавить участника на проект
+router.post("/:projectId/team", async (req, res) => {
+    const actualRole = await ProjectTeamMember.findOne({
+        where: {
+            projectId: req.params.projectId,
+            userId: req.body.user.userId,
+            finishedAt: null
+        }
+    });
+    if (actualRole.roleCode < 3) {
+        // на проекте только 1 роль - проверка что ещё нет такого
+        const whereRequest = {};
+        const login = req.body.login;
+        if(login){
+            whereRequest.login = login;
+        }
+        const email = req.body.email;
+        if(email){
+            whereRequest.email = email;
+        }
+        const memberUser = await User.findOne({
+            where: whereRequest
+        });
+        if(memberUser) {
+            const newMember = await ProjectTeamMember.create({
+                updatedAt: Date.now(),
+                projectId: req.params.projectId,
+                adminId: req.body.user.userId,
+                userId: memberUser.userId,
+                roleCode: req.body.roleCode,
+            });
+            res.send().status(200);
+        }
+        else {
+            const err = new Error("Вы пытаетесь добавить несуществующего пользователя");
+            res.status(400);
+            res.send(err).status(400);
+        }
+    }
+    else {
+        const err = new Error("У вас нет прав доступа на добавление участников команды");
+        res.status(400);
+        res.send(err).status(400);
+    }
+});
+
+// изменить роль участника на проекте
+router.put("/:projectId/team/:userId", async(req, res) => {
+    const actualRole = await ProjectTeamMember.findOne({
+        where: {
+            projectId: req.params.projectId,
+            userId: req.body.user.userId,
+            finishedAt: null
+        }
+    });
+    if (actualRole.roleCode < 3) {
+        // добавить проверку, что пытаются поменять не роль владельца
+        const member = await ProjectTeamMember.findOne({
+            where:{
+                userId: req.params.userId,
+                finishedAt: null
+            }
+        });
+        await member.update({
+            updatedAt: Date.now(),
+            roleCode: req.body.roleCode,
+        });
+        res.send(member).status(200);
+    }
+    else {
+        const err = new Error("У вас нет прав доступа на изменение ролей участников команды");
+        res.status(400);
+        res.send(err).status(400);
+    }
+});
+
+// удалить участника с проекта
+router.delete("/:projectId/team/:userId", async(req, res) => {
+    const actualRole = await ProjectTeamMember.findOne({
+        where: {
+            projectId: req.params.projectId,
+            userId: req.body.user.userId,
+            finishedAt: null
+        }
+    });
+    if (actualRole.roleCode < 3) {
+        // добавить проверку, что пытаются удалить не владельца, и что админа удаляет владелец
+        const deletedMember = await ProjectTeamMember.findOne({
+            where:{
+                userId: req.params.userId,
+                finishedAt: null
+            }
+        })
+        await deletedMember.update({
+            updatedAt: Date.now(),
+            finishedAt: Date.now(),
+        });
+        res.send().status(200);
+    }
+    else {
+        const err = new Error("У вас нет прав доступа на удаление участников этого проекта");
+        res.status(400);
+        res.send(err).status(400);
+    }
+});
 
 export default router
