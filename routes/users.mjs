6eba4d4849da525a87167bb2550e973d641dc3f8
kmn04 <21zaarin01@gmail.com>
2023-05-20 23:15:02 +0300

    инфа о юзере в профиле

--- routes/users.mjs
+++ routes/users.mjs
@@ -1,8 +1,86 @@
 import express from 'express';
+import {User} from "../models/User.mjs";
 
 // каскадное удаление проектов, перевод задач на автора задачи
-// получение всех пользователей на проекте
 
 const router = express.Router();
+
+// получение информации о пользователе
+router.get("/", async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            userId: req.body.user.userId,
+            deletedAt: null
+        }
+    });
+    if (user){
+        const preparedResult = {
+            userId: req.body.user.userId,
+            secondName: user.secondName,
+            name: user.firstName,
+            middleName: user.thirdName ?? '',
+            login: user.login,
+            email: user.email
+        };
+        res.send(preparedResult).status(200);
+    }
+    else {
+        const err = new Error("Такого пользователя не существует");
+        res.status(400);
+        res.send(err).status(400);
+    }
+});
+
+// создание пользователя происходит при регистрации
+
+// обновление информации о пользователе
+router.put("/", async (req, res) => {
+    const userInfo = await User.findOne({
+        where: {
+            userId: req.body.user.userId,
+            deletedAt: null
+        }
+    });
+    if (userInfo){
+        const {user, ...newBody} = req.body;
+        await userInfo.update(newBody);
+        const preparedResult = {
+            userId: req.body.user.userId,
+            secondName: userInfo.secondName,
+            name: userInfo.firstName,
+            middleName: userInfo.thirdName ?? '',
+            login: userInfo.login,
+            email: userInfo.email
+        };
+        res.send(preparedResult).status(200);
+    }
+    else {
+        const err = new Error("Такого пользователя не существует");
+        res.status(400);
+        res.send(err).status(400);
+    }
+});
+
+// удаление аккаунта
+router.delete("/", async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            userId: req.body.user.userId,
+            deletedAt: null
+        }
+    });
+    if (user){
+        await user.update({
+            deletedAt: Date.now()
+            // авто логаут
+        });
+        res.send().status(200);
+    }
+    else {
+        const err = new Error("Такого пользователя не существует");
+        res.status(400);
+        res.send(err).status(400);
+    }
+});
 
 export default router
